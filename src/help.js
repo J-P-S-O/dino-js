65a8a6bf2febb556b1a25a9b359a5f72b7eac1f9
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-08 11:45:12 -0300

    .

--- src/help.js
+++ src/help.js
@@ -1,8 +1,11 @@
-export default function(){
+import chalk from 'chalk'
+export default function(args){
 let help = `
 	${chalk.bold("new")} <dir> : Create app from template to <dir>\n
 	${chalk.bold("start")} <dir>: Start app in <dir> or current folder
 
+	To get help for a command, run Dino <command> --help
 `
+
 
 }
