2549e705c9605bd7b6e880745304e20b85c24399
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-08 11:26:46 -0300

    .

--- src/help.js
+++ src/help.js
@@ -1,3 +1,8 @@
 export default function(){
-	
+let help = `
+	${chalk.bold("new")} <dir> : Create app from template to <dir>\n
+	${chalk.bold("start")} <dir>: Start app in <dir> or current folder
+
+`
+
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 process.title = "dino"
-if (args._.length == 2) showHelp();
-console.log(chalk.yellow('This is beta software'))
 import chalk from "chalk"
 import minimist from "minimist"
 import genDir from "./gendir.js"
 import showHelp from "./help.js"
 let args = minimist(process.argv)
-let m = "node" == process.argv[0] ? 1 : 0
-if ( args._[2+m] == "new"){
+if (args._.length == 2 || args.help == true) showHelp(args);
+console.log(chalk.yellow('This is beta software'))
+
+if ( args._[3] == "new"){
 	genDir(args)
 }
