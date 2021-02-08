e1b9835b954a06ae3101cd35735cc14f0ecafb05
J-p-s-o <jpso.guerreiro@gmail.com>
2021-02-08 08:09:51 -0300

    .

--- installer/index.ts
+++ installer/index.ts
@@ -4,21 +4,25 @@ import { spawn } from "child_process"
 console.log(chalk.green("About to install dino"))
 let pdir = process.cwd()
 let ddir = process.env.ProgramFiles + "\\dino"
+console.log(chalk.yellow("Requires Node.js 12+ and Git"))
 if (!fs.existsSync(ddir)){
     fs.mkdirSync(ddir);
     process.chdir(ddir);
-    let git = spawn("git","clone", "https://github.com/j-p-s-o/dino-js/tree/latest")
+    let git = spawn("git","clone", "https://github.com/j-p-s-o/dino-js/tree/latest", ".")
     
    git.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+  console.log(`${data}`);
 });
 
 git.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+  console.error(`${data}`);
 });
 
 git.on('close', (code) => {
-  console.log(`Git exited with code ${code}`);
+  console.log(chalk.green(`Git exited with code ${code}`));
+  
+process.env.PATH = ddir +";"+process.env.PATH
+
 });
 	
 }
