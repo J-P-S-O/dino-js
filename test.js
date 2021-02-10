ed68dddd038e9c0ba9c512542e4e6e03acde9b3f
jp <jpso.guerreiro@gmail.com>
2021-02-10 09:48:40 -0300

    .

--- test.js
+++ test.js
@@ -1,9 +1,23 @@
 console.log('Deps check');
 import chalk from "chalk";
+import minimist from "minimist";
 console.log("Importing files...");
-process.on("uncaughtException", function (e) {
+process.on("error", function (e) {
     console.log(e.message);
     console.log("Dino exiting with " + chalk.red(1));
 });
+import gendir from "./src/gendir.js";
+import help from "./src/help.js";
+import start from "./src/start.js";
+import thrown from "./src/thrown.js";
+import touch from "./src/touch.js";
+var app = {
+    gendir: gendir,
+    help: help,
+    start: start,
+    thrown: thrown,
+    touch: touch,
+    minimist: minimist
+};
 console.log("Process exited with code " + chalk.green("0"));
 //# sourceMappingURL=test.js.map
--- test.ts
+++ test.ts
@@ -5,14 +5,21 @@ import chalk from "chalk"
 import minimist from "minimist"
 
 console.log("Importing files...")
-process.on("uncaughtException",(e)=>{
+process.on("error",(e)=>{
 	console.log(e.message)
 	console.log("Dino exiting with "+chalk.red(1))
 })
-import gendir from "./src/gendir"
-import help from "./src/help"
-import start from "./src/start"
-import thrown from "./src/thrown"
-import touch from "./src/touch"
-
+import gendir from "./src/gendir.js"
+import help from "./src/help.js"
+import start from "./src/start.js"
+import thrown from "./src/thrown.js"
+import touch from "./src/touch.js"
+let app = {
+	gendir,
+	help,
+	start,
+	thrown, 
+	touch,
+	minimist
+}
 console.log("Process exited with code "+ chalk.green("0"))
